f0d63cac2229c7d2147d6044d5e5a8a24767a67d
cobra0798 <cobra0798@192.168.0.3>
2016-05-02 18:19:13 -0400

    Items class partial implementation

---
+++ Hay Day calculator/src/hayDay/xmlNameObjectPairings/Items.java
@@ -0,0 +1,36 @@
+package hayDay.xmlNameObjectPairings;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+
+import hayDay.xmlObjects.HayDayType;
+import hayDay.xmlObjects.Item;
+
+public abstract class Items
+{
+    private HayDayType hayDay;
+    private ArrayList<Item> items;
+    
+    public Items(HayDayType hayDay, ArrayList<Item> items)
+    {
+        this.hayDay = hayDay;
+        this.items = items;
+    }
+    
+    public abstract LinkedHashMap<String, Item> getItems();
+    
+    public LinkedHashMap<String, Double> getCostsForOne()
+    {
+        
+    }
+    
+    public LinkedHashMap<String, Double> getCostsForTen()
+    {
+        
+    }
+    
+    public LinkedHashMap<String, Integer> getLevels()
+    {
+        
+    }
+}
