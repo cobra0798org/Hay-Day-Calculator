d0709bd8e1cc9d45b00522a1b7a8574fd15f63b1
cobra0798 <cobra0798@hotmail.com>
2016-05-08 11:50:48 -0400

    Machine's Product element; contains type class & product name

--- Hay Day calculator/src/hayDay/xmlObjects/Product.java
+++ Hay Day calculator/src/hayDay/xmlObjects/Product.java
@@ -2,14 +2,29 @@ package hayDay.xmlObjects;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlValue;
 
 @XmlAccessorType(XmlAccessType.FIELD)
 public class Product
 {
+    @XmlAttribute(name = "type", required = true)
+    protected String type;
     @XmlValue
     protected String product;
     
+    public Class<?> getType()
+    {
+        try
+        {
+            return Class.forName("hayDay.xmlObjects." + type);
+        }
+        catch (ClassNotFoundException e)
+        {
+            e.printStackTrace();
+            return null;
+        }
+    }
     public String getProductName()
     {
         return product;
