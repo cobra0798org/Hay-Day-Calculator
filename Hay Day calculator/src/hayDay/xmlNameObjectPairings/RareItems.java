8d00b56ad6a57e4fe86cd687b3a2d91a4ec55623
cobra0798 <cobra0798@hotmail.com>
2016-05-07 20:52:34 -0400

    added Items extension to RareItems

--- Hay Day calculator/src/hayDay/xmlNameObjectPairings/RareItems.java
+++ Hay Day calculator/src/hayDay/xmlNameObjectPairings/RareItems.java
@@ -6,16 +6,18 @@ import java.util.LinkedHashMap;
 import hayDay.xmlObjects.HayDayType;
 import hayDay.xmlObjects.Item;
 
-public class RareItems
+public class RareItems extends Items
 {
     private ArrayList<Item> rareItems;
     
     public RareItems(HayDayType hayDay)
     {
+        super(hayDay.getRareItems());
         rareItems = hayDay.getRareItems();
     }
     
-    public LinkedHashMap<String, Item> getRareItems()
+    @Override
+    public LinkedHashMap<String, Item> getItems()
     {
         LinkedHashMap<String, Item> items = new LinkedHashMap<String, Item>();
         for (Item item : rareItems)
@@ -24,38 +26,5 @@ public class RareItems
         }
         
         return items;
-    }
-    
-    public LinkedHashMap<String, Double> getCostsForOne()
-    {
-        LinkedHashMap<String, Double> prices = new LinkedHashMap<String, Double>();
-        for (Item item : rareItems)
-        {
-            prices.put(item.getName(), item.getCostForOne());
-        }
-        
-        return prices;
-    }
-    
-    public LinkedHashMap<String, Double> getCostsForTen()
-    {
-        LinkedHashMap<String, Double> costsForTen = new LinkedHashMap<String, Double>();
-        for (Item item : rareItems)
-        {
-            costsForTen.put(item.getName(), item.getCostForTen());
-        }
-        
-        return costsForTen;
-    }
-    
-    public LinkedHashMap<String, Integer> getLevels()
-    {
-        LinkedHashMap<String, Integer> levels = new LinkedHashMap<String, Integer>();
-        for (Item item : rareItems)
-        {
-            levels.put(item.getName(), item.getLevel());
-        }
-        
-        return levels;
     }
 }
