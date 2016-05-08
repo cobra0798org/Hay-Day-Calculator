371c2e86157f4a7646751393045e90ae30dc37bb
cobra0798 <cobra0798@hotmail.com>
2016-05-08 11:51:50 -0400

    getSource() returns the item reference instead of a String

--- Hay Day calculator/src/hayDay/xmlObjects/Source.java
+++ Hay Day calculator/src/hayDay/xmlObjects/Source.java
@@ -6,6 +6,11 @@ import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlTransient;
 import javax.xml.bind.annotation.XmlValue;
 
+import hayDay.Bushes;
+import hayDay.xmlNameObjectPairings.Animals;
+import hayDay.xmlNameObjectPairings.Machines;
+import hayDay.xmlNameObjectPairings.Trees;
+
 @XmlAccessorType(XmlAccessType.FIELD)
 public class Source
 {
@@ -34,8 +39,30 @@ public class Source
         return classType;
     }
     
-    public String getSource()
+    public Object getSource(HayDayType hayDay)
     {
-        return this.source;
+        Object source = null;
+        System.out.println(this.source);
+        if(classType == Machine.class)
+        {
+            Machines machines = new Machines(hayDay);
+            source = machines.getMachine(this.source);
+        }
+        else if(classType == Animal.class)
+        {
+            Animals animals = new Animals(hayDay);
+            source = animals.getAnimal(this.source);
+        }
+        else if(classType == Tree.class)
+        {
+            Trees trees = new Trees(hayDay);
+            source = trees.getTree(this.source);
+        }
+        else if(classType == Bush.class)
+        {
+            Bushes bushes = new Bushes(hayDay);
+            source = bushes.getBush(this.source);
+        }
+        return source;
     }
 }
